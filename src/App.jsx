cecc3333534b6a1824d54e0e20abec009c093ff7
samyleedev <68584701+samyleedev@users.noreply.github.com>
2022-11-17 12:54:53 +0100

    add footer and responsivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Footer from "./components/footer/Footer";
 import QuizContainer from "./components/quiz-container/QuizContainer";
 
 function App() {
   return (
     <div className="App">
       <QuizContainer />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>
+        created by <span>samyleeDev</span> - devChallenges.io
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
